851cdafee34feddf8e3e6a24b6f4eded76534b1f
Cagrisimsekk <133535265+Cagrisimsekk@users.noreply.github.com>
2023-10-06 23:35:44 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    "./node_modules/tw-elements/dist/js/**/*.js"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins'],
+     }
+    },
+  },
+  plugins: []
+}
